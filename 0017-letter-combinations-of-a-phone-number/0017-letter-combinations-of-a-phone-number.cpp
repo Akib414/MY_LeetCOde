6122ebeed2c68ddf82753935d97c62ebe2810e21
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-07-04 14:20:22 +0600

    Time: 3 ms (38.02%), Space: 8.3 MB (32.46%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    private:
-    void comb(int index , string &digits ,map<char,string>key, string &output , vector<string>&ans){
-        if(index >= digits.size()){
-             ans.push_back(output);
+    void process(string* arr, int n, string digits , string &output, vector<string>&ans){
+        if(n>=digits.size()){
+            ans.push_back(output);
             return ;
         }
-        string now_at = key[digits[index]];
-        for(auto a : now_at){
-              output.push_back(a);
-              comb(index+1,digits,key,output,ans);
-              output.pop_back();
+        int abcd = digits[n]-'0';
+        string s = arr[abcd];
+        for(int i = 0; i<s.size();i++){
+            output.push_back(s[i]);
+            process(arr,n+1,digits,output,ans);
+             output.pop_back();
         }
     }
 public:
     vector<string> letterCombinations(string digits) {
-        map<char,string>key ={{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
-    vector<string>ans ;
-    if(digits.size() == 0) return ans;
-    string output;
-    int index = 0;
-    comb(index , digits , key, output , ans);
-    return ans;
+        string arr[10] ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string output;
+        vector<string>ans={};
+        if(digits.size()==0) return ans;
+        process(arr,0,digits,output,ans);
+        return ans ;
+
     }
 };
