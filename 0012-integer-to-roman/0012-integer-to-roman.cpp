65a1aa0bbd80d7451fd58cac3c6ad43d4b75c1a4
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-03 15:42:14 +0600

    Time: 4 ms (73.67%), Space: 8.3 MB (90.38%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,71 +1,57 @@
 class Solution {
 public:
- 
- 
-     void rom(string &s,int num , int i){
-        if(num==0) return ;
-        int ld = num%10;
-        rom(s,num/10,i+1);
-        if(i==3){
-            for(int j = 1 ; j<=ld; j++)
-            s+="M";
-        }
-
-
-        else if(i==2 && ld >=5 && ld<9){
-            for(int j = 5 ; j<=ld ; j++){
-                if(j==5) s+="D";
-                else s+="C";
-            } 
-        }
-        else if(i==2 && ld==9) s+="CM";
-
-            else if(i==2 && ld <4 && ld>=1){
-            for(int j = ld ; j>=1 ; j--)
-              s+="C";
-            
-        }
-        else if(i==2 && ld==4) s+="CD";
-
-
-        else if(i==1 && ld>=5 && ld<9){
-            for(int j = 5; j<=ld ; j++){
-                if(j==5) s+="L";
-                else s+="X";
-            }
-        }
-        else if(i==1 && ld==9) s+="XC";
-
-                    else if(i==1 && ld <4 && ld>=1){
-            for(int j = ld ; j>=1 ; j--)
-              s+="X";
-            
-        }
-        else if(i==1 && ld==4) s+="XL";
-
-
-           else if(i==0 && ld>=5 && ld<9){
-            for(int j = 5; j<=ld ; j++){
-                if(j==5) s+="V";
-                else s+="I";
-            }
-        }
-        else if(i==0 && ld==9) s+="IX";
-
-                    else if(i==0 && ld <4 && ld>=1){
-            for(int j = ld ; j>=1 ; j--)
-              s+="I";
-            
-        }
-        else if(i==0 && ld==4) s+="IV";
-
-
-       
-     }
     string intToRoman(int num) {
-         string s;
-         int i = 0;
-         rom(s,num,i);
-         return s;
+        string ans = "" ;
+        while(num>0){
+            
+
+
+            if(num/1000 > 0){
+                int ld = num/1000;
+             for(int i = 0 ; i<ld ;i++)
+             ans+="M";
+             num = num%1000;
+            }
+
+
+            else if(num/100 > 0){
+                   int ld = num/100;
+                if(ld == 4) ans+="CD";
+                else if(ld == 9) ans+="CM";
+                else if(ld>=5) ans+="D";
+                if(ld != 4 && ld != 9){
+                    for(int i = 1 ; i<=ld%5 ; i++)
+                    ans+="C";
+                }
+                num =num%100;
+            }
+
+            else if(num/10 > 0){
+                   int ld = num/10;
+                if(ld == 4) ans+="XL";
+                else if(ld == 9) ans+="XC";
+                else if(ld>=5) ans+="L";
+                if(ld != 4 && ld != 9){
+                    for(int i = 1 ; i<=ld%5 ; i++)
+                    ans+="X";
+                }
+                num = num % 10 ;
+            }
+
+            else if(num/1 > 0){
+                   int ld = num/1;
+                if(ld == 4) ans+="IV";
+                else if(ld == 9) ans+="IX";
+                else if(ld>=5) ans+="V";
+                if(ld != 4 && ld != 9){
+                    for(int i = 1 ; i<=ld%5 ; i++)
+                    ans+="I";
+                }
+                num = num % 1; 
+                
+            }
+           
+        }
+        return ans;
     }
 };
