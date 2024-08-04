e5a4a959e2fa7c423a3153cffea5b00a65b2fb63
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-04 22:49:09 +0600

    Time: 3 ms (66.52%), Space: 12.2 MB (30.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& v, int target) {
-        int m = v.size();
-        int n = v[0].size();
-        int s = 0, e = m-1;
-        int ans = 0;
+     int fr(vector<vector<int>>& matrix, int target , int m , int n){
+        int s = 0 , e = m-1;
         while(s<=e){
             int mid = s+(e-s)/2;
-           if(v[mid][0] <= target && v[mid][n-1]>= target){
-            ans = mid;
-            break ; 
-           }
-           else if(v[mid][0]>target) e = mid-1;
-           else if(v[mid][n-1]<target) s= mid+1;
+            if(matrix[mid][0] <= target && matrix[mid][n-1] >= target )
+            return mid ;
+            else if(matrix[mid][0] < target && matrix[mid][n-1] < target)
+            s = mid+1;
+            else if(matrix[mid][0] > target && matrix[mid][n-1] > target)
+            e = mid-1;
         }
-        int lo = 0, hi = n-1;
-        while(lo<=hi){
-            int mid = hi+(lo-hi)/2;
-            if(v[ans][mid] == target ) return true;
-            else if(v[ans][mid] < target ) lo = mid+1;
-            else hi = mid - 1;
+        return 0 ;
+     }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = fr(matrix,target,m,n);
+        cout<<row<<endl;
+        int s = 0 , e = n-1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            if(matrix[row][mid] == target) return true ;
+            else if(matrix[row][mid] > target) e = mid-1;
+            else s = mid+1;
         }
         return false;
     }
