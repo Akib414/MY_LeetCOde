048f679a26eee3d4bafaf20bcaf61684a17dd1e2
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-04 20:16:33 +0600

    Time: 9 ms (20.91%), Space: 11.7 MB (39.94%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    void ansify(stack<string>&st , string &ans){
-        if(st.size() == 0) return ;
-        string x = st.top();
-        st.pop();
-        ansify(st,ans);
-        ans = ans + "/" + x;
-    }
+    string simplifyPath(string path) {
 
-    
+        stack<string>store;
+        string s = "";
+        for(int i = 1 ; i < path.size() ; i++){
+            if(path[i] == '/'){
+                if(s != "." && s != ".." && s != "")
+                store.push(s);
+                if(!store.empty() && s == "..") 
+                store.pop();
+                s = "";
+            }
+            
+            else{
+            s+=path[i];
+            }
+           
 
+        } 
+  
+                if(!store.empty() && s == "..") store.pop();
+                  else if(s != "." && s != "" && s != "..")
+            store.push(s);
 
+            if(store.empty()) return "/";
 
-    string simplifyPath(string s) {
-        stack<string>st;
-    for(int i = 0 ; i < s.size() ; i++){
-        if(s[i]=='/') continue;
-        else{
-        string temp="";
-        while(s[i]!='/' && i<s.size()){
-         temp+=s[i];
-         i++;
+        string ans = "";
+        while(!store.empty()){
+           if(store.top() != "" )
+           ans = "/" + store.top() + ans;
+           store.pop();
         }
-        i--;
-        if( temp == ".."){
-            if(!st.empty())
-            st.pop();
-        }
-        else if(temp==".") continue;
-        else{
-            st.push(temp);
-        }
-        }
-    }
-
-    if(st.size() == 0) return "/";
-     string ans = "";
-     ansify(st,ans);
-    
-return ans;
+        return ans;
     }
 };
