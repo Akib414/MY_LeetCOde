bf15c1e7c357466e55b1ed7479038c1464831699
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-07 23:37:47 +0600

    Time: 79 ms (91.72%), Space: 62.1 MB (64.16%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,19 +1,22 @@
 class Solution {
-public:   
-    // void process(int n, int k,vector<int>&output,vector<vector<int> >&ans);
+public:
+  void ansify(int c,int n, int k , vector<int>&output, vector<vector<int>>&ans){
+    if(output.size() >= k){
+        ans.push_back(output);
+        return;
+    }
+    for(int i = c ; i <= n ; i++){
+        output.push_back(i);
+        ansify(i+1 ,n , k , output , ans );
+        output.pop_back();
+    }
+        
+  }
+
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int> >ans={{}};
-         for(int i=1 ; i<=n ; i++){
-            int s = ans.size();
-            for(int j=0;j<s;j++){
-                ans.push_back(ans[j]);
-               ans.back().push_back(i); 
-            }      
-         }
-         vector<vector<int> >final;
-         for(int i = 0 ; i<ans.size();i++){
-            if(ans[i].size() == k) final.push_back(ans[i]);
-         }
-        return final;
+        vector<vector<int>> ans ;
+        vector<int>output ;
+        ansify(1,n,k,output,ans);
+        return ans;
     }
 };
