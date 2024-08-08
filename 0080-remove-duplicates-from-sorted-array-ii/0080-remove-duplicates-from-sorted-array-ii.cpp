bfdb5d7c52405a2c7c4cc8314a43d5426c1f47b5
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-08 14:42:07 +0600

    Time: 8 ms (21.03%), Space: 13.7 MB (37.56%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,21 +4,23 @@ public:
         int cnt = 1 ;
         int curr = nums[0];
         int ans = 1;
+        int j = 1;
         for(int i = 1 ; i < nums.size() ; i++){
             if(nums[i] == curr){
                 cnt++;
-                if(cnt <= 2)
+                if(cnt <= 2){
                 ans++;
-                else
-                nums[i] = INT_MAX ;
+                nums[j++] = nums[i];
+                }
             }
             else{
                 cnt = 1 ;
                 ans++;
+                nums[j++] = nums[i];
                 curr = nums[i];
             }
         }
-        sort(nums.begin(),nums.end());
+   
         return ans;
     }
 };
