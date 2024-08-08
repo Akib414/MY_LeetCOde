bb7eb8e595a1eccde45ffb6d288deeca3658ab63
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-08 20:03:13 +0600

    Time: 5 ms (64.65%), Space: 12.3 MB (27.72%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>m;
-        for(auto a : nums) m[a]++;
-        for(auto a : m){
-            if(a.second == 1) return a.first ;
+        if(nums.size() == 1) return nums[0];
+        sort(nums.begin(),nums.end());
+        int i = 0;
+        while( i < nums.size()){
+            if(nums[i]==nums[i+1] && nums[i+1] == nums[i+2]){
+                i+=3 ;
+            }
+            else{
+                return nums[i];
+            }
+            if(i==nums.size()-1) {
+                return nums[i];
+            }
         }
         return -1;
     }
