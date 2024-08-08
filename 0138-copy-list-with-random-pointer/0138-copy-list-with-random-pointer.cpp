7092ad94e4c815268ea353403004adaca57fd337
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-08 23:58:34 +0600

    Time: 7 ms (51.08%), Space: 14.5 MB (38.43%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -15,67 +15,60 @@ public:
 */
 
 class Solution {
-
-
 public:
-    void instail(Node* &copyhead , Node* &copytail , int x){
-        if(copyhead == NULL){
-            copyhead = new Node(x);
-            copytail = copyhead ;
-            return;
-        }
-        Node* temp = new Node(x);
-        copytail->next = temp ;
-        copytail = temp ;
-    }
-
-    Node* copyRandomList(Node* head) {
-        if(head == NULL) return NULL;
-        Node* temp = head ;
-        Node* copyhead = NULL;
-        Node* copytail = NULL;
-        while(temp != NULL){
-          instail(copyhead,copytail,temp->val);
-         temp = temp->next;
-        }
-
-        
-        temp = head ;
-        Node* t;
-        Node* c = copyhead;
-        Node *cp ;
-
-        while(temp != NULL){
-         t = temp->next ;
-        temp->next = c ;
-        temp = t;
-        cp = c->next;
-        c->next = t ; 
-        c= cp;
-        }
-       
-       temp = head ;
-       c = copyhead;
-       while(temp != NULL && c != NULL){
-        if(temp->random != NULL)
-        temp->next->random = temp->random->next;
-        else
-         temp->next->random = NULL;
-         temp = temp->next->next;
-       }
-
-     temp = head ;
-     c = copyhead ;
-     while(temp != NULL && c != NULL){
-        temp->next = c->next ;
-        temp = temp->next ;
-        if(c->next != NULL)
-        c->next = temp->next ;
-        else
-        c->next = NULL;
-        c = c->next;
+     void instail( Node* &copyhead, Node* &copytail , int n){
+     if(copyhead == NULL){
+        copyhead = new Node(n);
+        copytail = copyhead ;
+        return ;
+     }
+     Node* temp = new Node(n);
+     copytail->next = temp ;
+     copytail = temp ;
      }
 
-       return copyhead;
+
+    Node* copyRandomList(Node* head) {
+   Node* temp = head ;
+   Node* copyhead = NULL;
+   Node* copytail = NULL;
+
+   while(temp != NULL){
+   instail(copyhead , copytail , temp->val);
+   temp = temp->next ;
+   }
+temp = head ;
+Node* copy = copyhead ;
+while(temp != NULL && copy != NULL){
+    Node* store = temp->next ;
+    Node* save = copy->next ;
+    temp->next =  copy ;
+    copy->next = store ;
+    temp = store ;
+    copy = save ;
+}
+temp  = head ;
+while(temp != NULL){
+    if(temp->random != NULL)
+    temp->next->random  = temp->random->next ;
+    else
+    temp->next->random  = NULL ;
+    temp = temp->next->next ; 
+}
+temp = head ;
+copy = copyhead ;
+while(temp != NULL && copy != NULL){
+    Node* store ;
+    if(temp->next->next != NULL)
+store = temp->next->next->next ;
+    else
+    store = NULL ;
+    temp->next = temp->next->next ;
+    copy->next = store ;
+    temp = temp->next ;
+    copy = copy->next ;
+}
+return copyhead ;
+
     }
 };
