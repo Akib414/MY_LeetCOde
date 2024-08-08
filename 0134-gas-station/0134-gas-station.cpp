64cad778ee413a852d74ffa7d88aec27228c24ff
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-08 19:48:35 +0600

    Time: 92 ms (86.15%), Space: 130.8 MB (43.36%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-              int bal = 0;
-              int front = 0;
-              int rear = 0;
-              int n = gas.size();
-              int j = 0;
-       
-              while(front < n){
-                bal = bal + gas[rear] - cost[rear];
-                
-                if(bal < 0) {
-                    
-                   if(rear < front) return -1;
-                   else {
-                    front = rear+1;
-                    rear = front ;
-                    j=0;
-                    bal = 0;
-                   }
+       long long int sum = 0;
+        int a = 0 ;
+        int i = 0;
+        int curr = 0;
+        
+while(curr<gas.size()){
+    if(a==1 && curr == i) return curr ;
+    sum = sum + gas[i] - cost[i];
+    if(sum < 0){
+       sum = 0 ;
+        if(a==0 ){
+            curr = i+1 ;
+        }
+        if(a==1) break ;
+    }
+    i++;
+    if(a==0 && i==gas.size()){
+        i = 0 ;
+        a = 1 ;
+    }
 
-                }
-                else{
-                    if(rear == n-1) rear = 0;
-                    else rear++;
-                      j++;
-                       if(rear == front && j==n){
-                    return front;
-                    break;   
-                       }
-                }
-              }
-                return -1;  
+}
+return -1;
+      
+       
     }
 };
