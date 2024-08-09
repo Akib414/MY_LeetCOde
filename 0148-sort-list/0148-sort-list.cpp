ed9beac0904de5243754003b37dd7aa43966a395
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-09 21:35:45 +0600

    Time: 167 ms (9.73%), Space: 74.9 MB (24%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,78 +1,42 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-
-    ListNode* findmid(ListNode* head){
-        ListNode* fast = head->next ;
-        ListNode* slow = head ;
-        while(fast != NULL && fast->next != NULL){
-            fast = fast->next->next ;
-            slow = slow->next ;
+    ListNode* merge(ListNode* a, ListNode* b) {
+        // merging two linked list with their head
+        ListNode*c=new ListNode(-1);
+        ListNode*temp=c;
+        while(a!=NULL && b!=NULL) {
+            if(a->val <= b->val) {
+                temp->next=a;
+                a=a->next;
+                temp=temp->next;
+            }
+            else{
+                temp->next=b;
+                b=b->next;
+                temp=temp->next;
+            }
         }
-        return slow ;
+        if(a==NULL) temp->next=b;
+        else temp->next=a;
+        return c->next;
     }
-    ListNode* merge(ListNode* left , ListNode* right ){
-        ListNode* sorted ;
-        if(left->val < right->val){
-           sorted = left ;
-           left = left->next ;
-        }
-        else{
-            sorted = right ;
-            right = right->next ;
-        }
-   ListNode* ans = sorted ;
-       while(left != NULL && right != NULL){
-        if(left->val < right->val){
-            sorted->next = left ;
-             sorted = left ;
-            left = left->next ;
-           
-        }
-        else{
-            sorted->next = right ;
-            sorted = right ;
-            right = right->next ;
-            
-        }
-       }
-        while(left != NULL){
-            sorted->next = left ;
-                         sorted = left ;
-
-            left = left->next ;
-           
-        }
-            while(right != NULL){
-            sorted->next = right ;
-            sorted = right ;
-            right = right->next ;
-        }
-        sorted->next = NULL;
-        return ans ;
-       }
-    
-
     ListNode* sortList(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head ;
-        //dividing into two linked list
-        ListNode* mid = findmid(head);
-        ListNode* left = head ;
-        ListNode* right = mid->next ;
-        mid->next = NULL ;
-        ListNode* newleft = sortList(left);
-        ListNode* newright = sortList(right);
-        sortList(right);
-        //sorting and merging
-        return merge(newleft,newright);
+        // base case
+        if(head==NULL || head->next==NULL) return head;
+        // find the left middle of the linked list
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){ // 1st -> Odd , 2nd -> even
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        // slow is at left middle now devide the linked list in two part
+        ListNode*a=head;
+        ListNode*b=slow->next;
+        slow->next=NULL;
+        a=sortList(a); // recursion
+        b=sortList(b); // recursion
+        ListNode*c=merge(a,b);
+        return c;
     }
 };
