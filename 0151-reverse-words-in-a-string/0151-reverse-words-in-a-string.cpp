c6847a557df0ee9238166a2b149f1bbeb477bb52
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-09 22:09:15 +0600

    Time: 12 ms (8.43%), Space: 51.2 MB (5%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-      void rev(string &ns,stringstream &str ,string w , int i , int l ){
-       if(i==l) return ;
-       str>>w;
-       rev(ns,str,w,i+1,l);
-       if(i==0)
-       ns+=w;
-       else
-       ns+=w+" ";
-      }
+    void ansify(int i , int l,stringstream &ss , string str , string &ans){
+        if(i==l) return ;
+        ss>>str ;
+        ansify(i+1,l,ss,str,ans);
+        if(i != 0)
+        ans = ans+str+" ";
+        else
+        ans = ans+str;
+
+    }
+
+
     string reverseWords(string s) {
-      stringstream ss(s);
-      string word ;
-      int l = 0;
-     while(ss>>word){
-        l++;
-     }
-    string ns ;
-    stringstream str(s);
-    int i = 0;
-    string w;
-    rev(ns,str,w,i,l);
-    return ns;
+        string ans;
+        string str;
+        stringstream ss(s);
+         int l = 0 ; 
+ while(ss >> str){
+     l++;
+ }
+    stringstream newss(s);
+        ansify(0,l,newss,str,ans);
+        return ans;
     }
 };
