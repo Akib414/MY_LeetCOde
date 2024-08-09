d13e7bdc23c1f3a6ddb83678967f4f87390d06e1
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-09 21:31:55 +0600

    Time: 548 ms (5.01%), Space: 55.8 MB (89.87%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,71 +10,69 @@
  */
 class Solution {
 public:
+
+    ListNode* findmid(ListNode* head){
+        ListNode* fast = head->next ;
+        ListNode* slow = head ;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next ;
+            slow = slow->next ;
+        }
+        return slow ;
+    }
+    ListNode* merge(ListNode* left , ListNode* right ){
+        ListNode* sorted ;
+        if(left->val < right->val){
+           sorted = left ;
+           left = left->next ;
+        }
+        else{
+            sorted = right ;
+            right = right->next ;
+        }
+   ListNode* ans = sorted ;
+       while(left != NULL && right != NULL){
+        if(left->val < right->val){
+            sorted->next = left ;
+             sorted = left ;
+            left = left->next ;
+           
+        }
+        else{
+            sorted->next = right ;
+            sorted = right ;
+            right = right->next ;
+            
+        }
+       }
+        while(left != NULL){
+            sorted->next = left ;
+                         sorted = left ;
+
+            left = left->next ;
+           
+        }
+            while(right != NULL){
+            sorted->next = right ;
+            sorted = right ;
+            right = right->next ;
+        }
+        sorted->next = NULL;
+        return ans ;
+       }
+    
+
     ListNode* sortList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr)
-            return head;
-
-        int length = getLength(head);
-        ListNode dummy(0);
-        dummy.next = head;
-
-        for (int step = 1; step < length; step *= 2) {
-            ListNode* curr = dummy.next;
-            ListNode* tail = &dummy;
-
-            while (curr) {
-                ListNode* left = curr;
-                ListNode* right = split(left, step);
-                curr = split(right, step);
-
-                tail = merge(left, right, tail);
-            }
-        }
-
-        return dummy.next;
-    }
-
-private:
-    int getLength(ListNode* head) {
-        int length = 0;
-        ListNode* curr = head;
-        while (curr) {
-            length++;
-            curr = curr->next;
-        }
-        return length;
-    }
-
-    ListNode* split(ListNode* head, int step) {
-        if (head == nullptr)
-            return nullptr;
-
-        for (int i = 1; i < step && head->next; i++) {
-            head = head->next;
-        }
-
-        ListNode* right = head->next;
-        head->next = nullptr;
-        return right;
-    }
-
-    ListNode* merge(ListNode* left, ListNode* right, ListNode* tail) {
-        ListNode* curr = tail;
-        while (left && right) {
-            if (left->val < right->val) {
-                curr->next = left;
-                left = left->next;
-            } else {
-                curr->next = right;
-                right = right->next;
-            }
-            curr = curr->next;
-        }
-
-        curr->next = left ? left : right;
-        while (curr->next)
-            curr = curr->next;
-
-        return curr;
+        if(head == NULL || head->next == NULL) return head ;
+        //dividing into two linked list
+        ListNode* mid = findmid(head);
+        ListNode* left = head ;
+        ListNode* right = mid->next ;
+        mid->next = NULL ;
+        ListNode* newleft = sortList(left);
+        ListNode* newright = sortList(right);
+        sortList(right);
+        //sorting and merging
+        return merge(newleft,newright);
     }
 };
