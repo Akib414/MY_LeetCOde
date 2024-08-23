7c95aedaf07ebe7b902d785f7b3338a0033d77e5
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-23 17:25:15 +0600

    Time: 12 ms (62.99%), Space: 27.9 MB (14.87%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,18 +11,12 @@
  */
 class Solution {
 public:
-      int findin(vector<int>& inorder , int element ,int instart , int inend){
-        for(int i = instart ; i <= inend ; i++){
-            if(inorder[i] == element) return i ;
-        }
-        return -1;
-      }
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder ,int &prestart,int preend,int instart, int inend,  unordered_map<int,int> m){
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder ,int &prestart,int preend,int instart, int inend, map<int,int>&m){
         if(prestart > preend) return NULL ;
         if(instart>inend) return NULL ;
         int element = preorder[prestart++];
         TreeNode* root = new TreeNode(element);
-        int index = findin(inorder , element,instart , inend);
+        int index = m[element] ;
         
         TreeNode* lft = solve(preorder , inorder , prestart , preend , instart , index-1 , m) ;
         TreeNode* rgt = solve(preorder , inorder , prestart , preend , index+1 , inend , m) ;
@@ -38,10 +32,12 @@ public:
        int preend = preorder.size()-1 ;
        int instart = 0 ;
        int inend = inorder.size()-1;
-       unordered_map<int,int> m ;
-    //    for(int i = 0 ; i <= inend ; i++)
-    //    m[inorder[i]] = i ;
+       cout<<preorder.size();
+       map<int,int> m ;
+       for(int i = 0 ; i <= inend ; i++)
+       m[inorder[i]] = i ;
        TreeNode* ans = solve(preorder , inorder , prestart , preend , instart , inend , m);
+
        return ans ;
     }
 };
