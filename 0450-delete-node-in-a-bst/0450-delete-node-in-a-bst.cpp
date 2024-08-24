307d61352eee451c7909a79d0ccd41c95fcb42e9
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-24 22:41:24 +0600

    Time: 27 ms (37.99%), Space: 33.6 MB (18.75%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findmin(TreeNode* root){
+        if(root->left == NULL) return root->val ;
+        return findmin(root->left);
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL) return NULL ;
+        if(key > root->val){
+            root->right = deleteNode(root->right , key);
+            return root ;
+        }
+        else if(key < root->val){
+            root->left = deleteNode(root->left , key);
+            return root ;
+        }
+        else{
+        //if both sides dosent exist ;
+            if(root->left == NULL && root->right == NULL){
+            delete root ;
+            return NULL ;
+            }
+        //if one side exist then store that side and delete parent and return child ;
+            else if(root ->left != NULL && root->right == NULL){
+                TreeNode* temp = root->left ;
+                delete root ;
+                return temp ;
+            }
+            else if(root ->right != NULL && root->left == NULL){
+                TreeNode* temp = root->right ;
+                delete root ;
+                return temp ;
+            }
+            //if both side exists 
+            //either find max in the left side 
+            //or min in the right side 
+            //here max in the right side 
+            else{
+              int min_from_right = findmin(root->right) ;
+              root->val = min_from_right ;
+              root->right = deleteNode(root->right , min_from_right );
+              return root ;
+            }
+        }
+        
+    }
+};
