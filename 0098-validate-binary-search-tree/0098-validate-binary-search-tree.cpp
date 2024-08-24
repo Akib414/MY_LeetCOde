aeca45c1707c3371c1a91101643501b8f7df2d0b
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-25 01:04:36 +0600

    Time: 7 ms (75.61%), Space: 20.4 MB (63.33%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,8 +11,9 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root ,long long int mini ,long long  int maxi){
+    bool solve(TreeNode* root , long long int mini , long long int maxi){
         if(root == NULL) return true ;
+
         if(root->val > mini && root->val < maxi){
             return solve(root->left , mini , root->val ) && solve(root->right , root->val , maxi);
         }
@@ -20,8 +21,6 @@ public:
     }
 
     bool isValidBST(TreeNode* root) {
-        long long  int maxi = 1000000000000 ;
-        long long int mini =  -1000000000000;
-        return solve(root ,mini  , maxi);
+        return solve(root , -100000000000 , 100000000000);
     }
 };
