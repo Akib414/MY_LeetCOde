00bf1e7100fee164571a8f91f0f570d8fb56556b
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-25 16:52:02 +0600

    Time: 761 ms (5.08%), Space: 666.3 MB (5.28%) - LeetHub

---
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     
+      void traverse (TreeNode* root , vector<int>&inorder ){
+        if(root == NULL) return ;
+        traverse(root->left , inorder);
+        inorder.push_back(root->val);
+        traverse(root->right , inorder);
+      }
+
+      TreeNode* make(vector<int>inorder , int s , int e){
+        if(s > e) return NULL ;
+        //find mid 
+        int m = s+(e-s)/2 ;
+        //make a node
+        TreeNode* balanced = new TreeNode(inorder[m]);
+        //left part 
+        balanced->left = make(inorder , s , m-1);
+        //right part 
+        balanced->right = make(inorder , m+1 , e);
+        return balanced ;
+      }
+
+    TreeNode* balanceBST(TreeNode* root) {
+        //store into vector in inorder format ;
+        vector<int>inorder;
+        traverse(root,inorder);
+        //process
+        TreeNode* balanced = make(inorder , 0 , inorder.size()-1);
+        return balanced ;
+        
+    }
+};
