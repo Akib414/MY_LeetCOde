ae0d3a8bec173325b3752737811daf2051ae55e4
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-26 21:29:23 +0600

    Time: 120 ms (95.24%), Space: 101.8 MB (73.19%) - LeetHub

--- 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/1475-maximum-sum-bst-in-binary-tree.cpp
@@ -26,21 +26,36 @@ public:
    
    info solve(TreeNode* root ,int &max_sum_bst ){
     if(root == NULL){
+        //first time maximum is INTMIN , minimum is INT_MAX , sum is 0 , and flag is true 
         return {INT_MIN , INT_MAX , 0 , true};
     }
     info lft = solve(root->left , max_sum_bst);
     info rgt = solve(root->right , max_sum_bst);
     info to_return;
-    
+    //new maximum will be maximum from right 
     to_return.maximum = max(root->val ,rgt.maximum);
+
+    //new minimum will be minimum from left ;
     to_return.minimum = min(root->val , lft.minimum);
+
+    //sum will be left + right + main
     to_return.sum = lft.sum + rgt.sum + root->val ;
+
+    // is both left and right is true and main is less than the minimum from right && main is greater than the maximum from left it is bst .
     if(lft.flag && rgt.flag && (root->val < rgt.minimum && root->val > lft.maximum))
     to_return.flag = true ;
     else
-    to_return.flag = false ;
+    to_return.flag = false ; 
+
+
+    //if only main is possible bst then it will upgrade max_sum_bst 
+    //if it is false and sum is being increased dosent matter
+    //as it is false in the down side , no matter how up we go on the tree , it will be false
+    //and if it is false then the sum keep being increased will not bother as it will not be upgraded if if its false
     if(to_return.flag)
     max_sum_bst = max(max_sum_bst , to_return.sum);
+
+
     return to_return ;
    }
 
