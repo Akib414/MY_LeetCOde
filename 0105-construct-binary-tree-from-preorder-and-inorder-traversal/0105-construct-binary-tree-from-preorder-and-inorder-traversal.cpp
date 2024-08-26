9879530fe093e2e3c566b2c9d47a162ab1ee2030
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-27 01:51:41 +0600

    Time: 10 ms (75.67%), Space: 26.8 MB (66.75%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,34 +11,28 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder ,int &prestart,int preend,int instart, int inend,unordered_map<int,int>&m){
-        if(prestart > preend) return NULL ;
-        if(instart>inend) return NULL ;
-        int element = preorder[prestart++];
-        TreeNode* root = new TreeNode(element);
-        int index = m[element] ;
-        
-        TreeNode* lft = solve(preorder , inorder , prestart , preend , instart , index-1 , m) ;
-        TreeNode* rgt = solve(preorder , inorder , prestart , preend , index+1 , inend , m) ;
-        root->left = lft ;
-        root->right = rgt ;
-        return root ;
+ 
+   int findval(vector<int>& inorder , int value , int st , int end){
+      for(int i = st ; i <= end ; i++){
+        if(inorder[i] == value) return i ;
+      }
+      return -1;
+   }
 
-     }
+   TreeNode*  solve(vector<int>& preorder, vector<int>& inorder , int &i , int st , int end , int n){
+    if(st>end || i>n) return NULL ;
+    int value = preorder[i++] ;
+    int pos = findval(inorder , value , st,end); 
+    cout<<pos<<" ";
+    TreeNode* root = new TreeNode(value);
+    root->left = solve(preorder , inorder ,i , st , pos-1 , n);
+    root->right = solve(preorder , inorder ,i , pos+1 , end  , n);
+    return root ;
+   }
 
-
-    TreeNode* buildTree(vector<int>& preorder, vector<int>&inorder) {
-
-       int prestart = 0 ;
-       int preend = preorder.size()-1 ;
-       int instart = 0 ;
-       int inend = inorder.size()-1;
-       cout<<preorder.size();
-       unordered_map<int,int> m ;
-       for(int i = 0 ; i <= inend ; i++)
-       m[inorder[i]] = i ;
-       TreeNode* ans = solve(preorder , inorder , prestart , preend , instart , inend , m);
-
-       return ans ;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int i = 0 ;
+        TreeNode* ans = solve(preorder , inorder ,i, 0 , inorder.size()-1 ,inorder.size()-1);
+        return ans ;
     }
 };
