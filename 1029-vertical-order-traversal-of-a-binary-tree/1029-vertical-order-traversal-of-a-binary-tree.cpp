62a4ad983ec53205a6302c9b9177ed74cf65aabe
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-27 21:13:55 +0600

    Time: 4 ms (56.12%), Space: 14.4 MB (69.65%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,55 +11,43 @@
  */
 class Solution {
 public:
-       void create_column(int column ,  TreeNode* root, unordered_map<TreeNode*,int>&p ) {
-        if(root == NULL){
-            return ;
-        }
-        p[root] = column ;
-        create_column(column-1 , root->left , p);
-        create_column(column+1 , root->right , p);
-    }
-    void create_vectors(TreeNode* root , map<int,vector<int>>&m , unordered_map<TreeNode*,int> p){
-        if(root == NULL) return ;
-        queue<TreeNode*>q ;
-        q.push(root) ;
-        while(!q.empty()){
-            int size = q.size();
-            map<int,vector<int>> pm ;
-            for(int i = 0 ; i < size ; i++){
-            TreeNode* f = q.front();
-            pm[p[f]].push_back(f->val);  
-            q.pop();
-            if(f->left) q.push(f->left);
-            if(f->right) q.push(f->right);
-            }
-            for(auto a : pm){
-                if(a.second.size() > 1){
-                    sort(a.second.begin(),a.second.end());
-                }
-                for(auto b : a.second){
-                    m[a.first].push_back(b);
-                }
-            }
-        }
-    }
-    
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        unordered_map<TreeNode*,int> p ;
-        //firstly map column number with respect to node on overy node ;
-        create_column(0 , root , p);
-        //then create a map according to column number's vector 
-        map<int,vector<int>>m ;
-        create_vectors(root, m , p) ;
-        vector<vector<int>>ans ;
-        for(auto a : m){
-            vector<int>temp ;
-            for(auto b : a.second){
-                temp.push_back(b);
+        vector<vector<int>>ans;
+        map<TreeNode*,int>col ;
+        map<int,vector<int>> m;
+        queue<TreeNode*>q ;
+        q.push(root);
+        col[root] = 0;
+        
+        while(!q.empty()){
+          int size = q.size();
+          map<int,vector<int>>temp ;
+          for(int j = 0 ; j < size ; j++){
+          TreeNode* f; 
+          f = q.front();
+            q.pop();
+            temp[col[f]].push_back(f->val);
+            if(f->left){
+                q.push(f->left);
+                int f_col = col[f];
+                col[f->left] = f_col - 1 ;
             }
-            ans.push_back(temp);
+            if(f->right){
+                q.push(f->right);
+                int f_col = col[f];
+                col[f->right] = f_col + 1 ;
+            }  
         }
-        return ans ;
+        for(auto a : temp){
+            if(a.second.size() > 1) sort(a.second.begin() , a.second.end());
+            for(auto b : a.second){
+                m[a.first].push_back(b);
+            }
+        }
+        }
+        for(auto a : m){
+         ans.push_back(a.second);
+        }
+        return ans;
     }
 };
