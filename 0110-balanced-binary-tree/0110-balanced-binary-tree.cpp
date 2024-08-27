99c217b1f39ce69b9a0dc31dead64a89bd79466a
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-27 11:25:42 +0600

    Time: 8 ms (72.16%), Space: 21.7 MB (84.15%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,29 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
+// class Solution {
+// public:
 
-    int solve(TreeNode* root , bool &ans){
-        if(root == NULL) return 0 ;
-        int lft = solve(root->left , ans);
-        int rgt = solve(root->right , ans);
-        if(abs(lft - rgt) > 1) ans = false ;
-        return max(lft, rgt)+1;
-    }
+//     int solve(TreeNode* root , bool &ans){
+//         if(root == NULL) return 0 ;
+//         int lft = solve(root->left , ans);
+//         int rgt = solve(root->right , ans);
+//         if(abs(lft - rgt) > 1) ans = false ;
+//         return max(lft, rgt)+1;
+//     }
 
-    bool isBalanced(TreeNode* root) {
-        bool ans = true ;
-        int height = solve(root , ans);
-        return ans ;
-    }
-};
+//     bool isBalanced(TreeNode* root) {
+//         bool ans = true ;
+//         int height = solve(root , ans);
+//         return ans ;
+//     }
+// };
 
 
 
 //another process 
 
-/*
+
 class Solution {
 public:
    pair<int,bool> solve(TreeNode* root){
@@ -52,4 +52,3 @@ public:
         return p.second;
     }
 };
-*/
