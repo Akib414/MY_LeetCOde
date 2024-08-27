cec90f08aaa355f668ee45b53065591eed4312d1
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-27 19:43:51 +0600

    Update 0799-minimum-distance-between-bst-nodes.cpp

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -26,10 +26,15 @@ public:
         if(root == NULL) return  ;
         solve(root->left,mini);
         solve(root->right,mini);
+
+       //AS IN BST THE MAXIMUM IN LEFT IS THE CLOSEST VALUE TO THE ROOT VALUE 
+       
         if(root->left) {
             int maximum_in_left = maxleft(root->left);
             mini= min(mini , abs(root->val - maximum_in_left));
         }
+
+       // AS IN BST THE MINIMUM IN RIGHT IS THE CLOSEST VALUE TO THE ROOT VALUE 
         if(root->right){
         int minimum_in_right = minright(root->right);
         mini = min(mini , abs(root->val - minimum_in_right));
@@ -43,3 +48,32 @@ public:
         return mini ;
     }
 };
+
+
+/*******************************ANOTHER PROCESS USING THE INORDER TRAVERSAL PROPERTY(STORES THE BST IN SORTED ORDER) ************************/
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int> &v){
+        if(root==NULL){
+            return;
+        }
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    int minDiffInBST(TreeNode* root) {
+        vector<int> v;
+        inorder(root,v);
+        int mini=INT_MAX;
+        for(int i=0;i<v.size()-1;i++){
+            mini=min(mini,v[i+1]-v[i]);
+        }
+        return mini;
+    }
+};
+
+
+
+
+
+
