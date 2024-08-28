21453cc7e27528bda8c1cd58b6f899e5c585b7ee
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-08-28 16:18:22 +0600

    Time: 36 ms (6.75%), Space: 33.5 MB (20.74%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,49 +11,50 @@
  */
 class Solution {
 public:
-    int findmin(TreeNode* root){
-        if(root->left == NULL) return root->val ;
-        return findmin(root->left);
-    }
-
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root == NULL) return NULL ;
+
         if(key > root->val){
-            root->right = deleteNode(root->right , key);
+            root->right = deleteNode(root->right , key) ;
             return root ;
         }
+
         else if(key < root->val){
-            root->left = deleteNode(root->left , key);
+            root->left =deleteNode(root->left , key);
             return root ;
         }
         else{
-        //if both sides dosent exist ;
-            if(root->left == NULL && root->right == NULL){
+
+           if(root->left == NULL && root->right == NULL){
             delete root ;
             return NULL ;
-            }
-        //if one side exist then store that side and delete parent and return child ;
-            else if(root ->left != NULL && root->right == NULL){
-                TreeNode* temp = root->left ;
-                delete root ;
-                return temp ;
-            }
-            else if(root ->right != NULL && root->left == NULL){
-                TreeNode* temp = root->right ;
-                delete root ;
-                return temp ;
-            }
-            //if both side exists 
-            //either find max in the left side 
-            //or min in the right side 
-            //here max in the right side 
-            else{
-              int min_from_right = findmin(root->right) ;
-              root->val = min_from_right ;
-              root->right = deleteNode(root->right , min_from_right );
-              return root ;
-            }
+           }
+
+           else if(root->left && root->right == NULL ){
+            TreeNode* temp = root->left ;
+            delete root ;
+            return temp ;
+           }
+
+           else if(root->right && root->left == NULL){
+            TreeNode* temp = root->right ;
+            delete root ;
+            return temp ;
+           }
+
+           else{
+             TreeNode* max_in_left = root->left;
+             while(max_in_left->right != NULL)
+                max_in_left = max_in_left->right ;
+        
+             int temp = root->val ;
+             root->val = max_in_left->val ;
+             max_in_left->val = temp ;
+             root->left = deleteNode(root->left , key);
+             return root ;  
+           }
         }
         
+         return root ;  
     }
 };
