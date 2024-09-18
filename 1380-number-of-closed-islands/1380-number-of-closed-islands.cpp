5ab8c7c7ed895814c1653b4c93c14fcdb9a4612d
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-09-18 22:58:13 +0600

    Time: 6 ms (91.51%), Space: 13 MB (34.74%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
@@ -1,38 +1,56 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
 public:
-    int closedIsland(vector<vector<int>>& grid) {
-                        vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
-        vector<pair<int,int>>dir = {{-1,0},{1,0},{0,-1},{0,1}};
-         int ans = 0 ;
-         int n = grid.size() ;
-         int m = grid[0].size() ;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 0 && vis[i][j] == false){
-                    bool flag = true ;
-                    queue<pair<int,int>>q ;
-                    q.push({i,j});
-                    vis[i][j] = true ;
-                    while(!q.empty()){
-                     auto f = q.front();
-                     q.pop();
-                     int si = f.first ;
-                     int sj = f.second ;
-                     if(si == n-1 || sj == m-1 || si == 0 || sj == 0) flag = false;
-                     for(auto a : dir){
-                        int ci = si + a.first ;
-                        int cj = sj + a.second ;
-                        
-                        if(ci >= 0 && cj >= 0 && ci < n && cj < m &&  grid[ci][cj] == 0 && vis[ci][cj] == false){
-                            q.push({ci,cj});
-                            vis[ci][cj] = true ;
-                        }
-                     }
-                    }
-                    if(flag) ans++ ;
-                }      
+    int n, m;
+    bool vis[105][105];
+    vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    bool valid(int ci, int cj)
+    {
+        if (ci >= 0 && ci < n && cj >= 0 && cj < m)
+            return true;
+        else
+            return false;
+    }
+    bool flag;
+    void dfs(int si, int sj, vector<vector<int>> &grid)
+    {
+        vis[si][sj] = true;
+        if (si == 0 || si == n - 1 || sj == 0 || sj == m - 1)
+            flag = false;
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = si + d[i].first;
+            int cj = sj + d[i].second;
+            if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == 0)
+            {
+                dfs(ci, cj, grid);
             }
         }
-        return ans ;
+    }
+    int closedIsland(vector<vector<int>> &grid)
+    {
+        memset(vis, false, sizeof(vis));
+        n = grid.size();
+        m = grid[0].size();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (!vis[i][j] && grid[i][j] == 0)
+                {
+                    flag = true;
+                    dfs(i, j, grid);
+                    if (flag == true)
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
