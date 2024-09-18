52d829dc594f6b6a983dc1152a087ba12eb263dd
Akib414 <154895863+Akib414@users.noreply.github.com>
2024-09-18 21:57:13 +0600

    Time: 19 ms (25.98%), Space: 31.9 MB (16.33%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
+        vector<pair<int,int>>dir = {{-1,0},{1,0},{0,-1},{0,1}};
+         int ans = 0 ;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for(int j = 0 ; j <grid[0].size() ; j++){
+                if(grid[i][j] == 1 && vis[i][j] == false){
+                       int cnt = 0 ;
+                    queue<pair<int,int>>q ;
+                    q.push({i,j});
+                    vis[i][j] = true ;
+                    while(!q.empty()){
+                        cnt++ ;
+                     auto f = q.front();
+                     q.pop();
+                     int si = f.first ;
+                     int sj = f.second ;
+                     for(auto a : dir){
+                        int ci = si + a.first ;
+                        int cj = sj + a.second ;
+                        if(ci >= 0 && cj >= 0 && ci < grid.size() && cj < grid[0].size() &&  grid[ci][cj] == 1 && vis[ci][cj] == false){
+                            q.push({ci,cj});
+                            vis[ci][cj] = true ;
+                        }
+                     }
+                    }
+                    ans = max(ans , cnt);
+                }
+                
+            }
+        }
+        return ans ;
+    }
+};
